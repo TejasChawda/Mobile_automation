14683500b1ccf2ba4e7d8606ed2a02ecb175c666
Admin <admin@Admins-MacBook-Pro.local>
2023-08-29 00:30:16 +0530

    third commit

--- src/main/java/org/example/screens/HomeScreen.java
+++ src/main/java/org/example/screens/HomeScreen.java
@@ -1,6 +1,12 @@
 package org.example.screens;
 
+import io.appium.java_client.AppiumDriver;
+
 public class HomeScreen {
+
+    public HomeScreen(AppiumDriver appiumDriver){
+
+    }
     public ProfileScreen navToProfile() {
     }
 }
